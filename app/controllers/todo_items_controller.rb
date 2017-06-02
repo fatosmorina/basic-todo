0faa3b34e11fc78f0790e67cfc3879c8a6be56bd
Fatos Morina <fatosi.morina@gmail.com>
2017-06-03 00:40:26 +0200

    Implement create method at TodoItemsController

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,2 +1,7 @@
 class TodoItemsController < ApplicationController
+
+  def create
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.create(params[:todo_item].permit(:content))
+  end
 end
